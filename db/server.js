0ed2639573e674265e5d87325a5a686f0e0797cf
imlavaraju <imlavaraju@gmail.com>
2024-06-24 11:43:13 +0530

    Updated .gitignore and added server files
    - Updated .gitignore
    - Added db.json to the db directory
    - Added package-lock.json to the db directory
    - Added package.json to the db directory
    - Added server.js to the db directory
    - Added vercel.json to the db directory
    - Added server.js to the root directory

---
+++ db/server.js
@@ -0,0 +1,21 @@
+// JSON Server module
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  // Add custom route here if needed
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
