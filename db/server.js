3e3f5262a3f4ddb7f9ba87b280ba82026cf11c33
LAVARAJU GORLI <141550385+imlavaraju@users.noreply.github.com>
2024-06-24 14:27:40 +0530

    Update server.js

--- db/server.js
+++ db/server.js
@@ -1,7 +1,7 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
